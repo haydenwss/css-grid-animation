2c92937335dd7e50e6aa94051e0f9c1c7fc4deed
wss <websuitestudio@gmail.com>
2021-11-08 17:14:11 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+function App() {
+  return (
+    <body>
+    <h1>Animated Grid</h1>
+    <section className="animated-grid">
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+      <div className="card"></div>
+    </section>
+
+    <section className="animated-grid">
+      <div className="card">a</div>
+      <div className="card">b</div>
+      <div className="card">c</div>
+      <div className="card">d</div>
+      <div className="card">e</div>
+      <div className="card">f</div>
+      <div className="card">g</div>
+      <div className="card">h</div>
+      <div className="card">i</div>
+      <div className="card">j</div>
+      <div className="card">k</div>
+      <div className="card">l</div>
+      <div className="card">main</div>
+    </section>
+    
+  </body>
+  );
+}
+
+export default App;
